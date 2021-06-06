b3e548488d1cee59cf7c4d0295597b0a5180c10c
raihaan123 <raihaan.usman@gmail.com>
2021-06-06 23:58:59 +0100

    typo

--- src/libraries/decorators/decorators.py
+++ src/libraries/decorators/decorators.py
@@ -1,4 +1,3 @@
-# Library of decorators using RocketPy (incl. not implemented, debug, etc)
+# Library of decorators used in RocketPy applications (incl. not implemented, debug, etc)
 # https://realpython.com/primer-on-python-decorators/ for reference
 # Maintained by Raihaan Usman
-
