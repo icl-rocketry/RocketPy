6871778694d56f043a12746bfe80d271ee57fe19
raihaan123 <raihaan.usman@gmail.com>
2021-06-23 20:37:50 +0100

    Changes

--- src/Deployer.py
+++ src/Deployer.py
@@ -4,11 +4,10 @@ ICLR ROCKETPY - DEPLOYER
 Maintained by Raihaan Usman and Krish Nair
 
 Designed to take a converged rocket design through the manufacturing process
-
-- Manufacturing Planner to help define the manufacturing plan + draft an assembly guide
-- Task Assigner inspired by the 'Responsible (group!) of engineers' philosophy
-- Dynamic Gannt chart to handle project timelines and track bottlenecks
-- Tool Tracker to ensure no overlapped plans of work
+    - Manufacturing Planner to help define the manufacturing plan + draft an assembly guide
+    - Task Assigner inspired by the 'Responsible (group!) of engineers' philosophy
+    - Dynamic Gannt chart to handle project timelines and track bottlenecks
+    - Tool Tracker to ensure no overlapping plans of work with the same tool dependency
 
 Planned Integrations
 - Teams & Excel via Power Apps / Power Automate
@@ -21,49 +20,48 @@ DOCUMENTATION
 
 Manufacturing Planner
 
-1. For each Component:
-    (a) Select/define the process(es) (i.e. roadmap from raw to final state)
-    (b) Define target duration, priority and complexity
-    (c) Save details to Component within Rocket object
+    1. For each Component:
+        (a) Select/define the manufacturing process(es) (i.e. roadmap from raw to final part)
+        (b) Define target duration, priority and complexity scores
+        (c) Save details to Component within Rocket object
 
-2. For each Interface:
-    (a) Select/define integration process(es) (i.e. roadmap from raw to final state)
-    (b) Define target duration, priority and complexity
-    (c) Save details to Interface within Rocket object
+    2. For each Interface:
+        (a) Select/define the integration process(es)
+        (b) Define target duration, priority and complexity scores
+        (c) Save details to the Interface object within Rocket object
 
-3. Select the engineering group responsible for each component and interface - Task Assigner
+    3. Task Assigner - Provisionally assign the engineering group responsible for each component and interface
 
-4. Order the integration tasks (i.e. for each interface) to create the assembly guide!
-
+    4. Order the integration tasks (i.e. fitting each interface) to construct the Assembly Guide!
 
 
 Dynamic Gannt chart / Calendar
 
-1. Order the Component manufacturing tasks by priority
-2. Order the Assembly/Integration tasks as defined in the Assembly guide
-3. For each task, check the Group.availability attribute to calculate the best provisional slots for the duration defined
-4. Update the calendars of all engineers + the overall vehicle timeline
-5. With the Gannt chart integration, update the Gannt chart
-6. Allow the respective engineers to adjust their own tasks manually (timing + room) and to effectively 'pin' them
-7. Re-run the above to reshuffle all floating tasks to avoid hardware conflicts, room occupancy, etc.
+    1. Order the component manufacturing tasks by priority
+    2. Order the integration tasks as defined in the Assembly Guide
+    3. For each task, check the Group.availability attribute to calculate provisional slots for the defined duration
+    4. Update the calendars of all engineers + the overall vehicle timeline
+    5. Update Gannt chart (with integration)
+    6. Allow respective engineers to adjust their own tasks manually (timing + room) and to 'pin' them
+    7. Re-run module to recalculate all floating tasks to avoid hardware conflicts, room occupancy limits, etc.
 
 
-
------------------------------
+-----
 TO-DO
+-----
 
 Manufacting Planner:
 - Modify Rocket, Component and Interface classes to store the manufacturing plans
-- Make the Engineer() and Group() classes and library - implements name, year, familiar tools, experience, availability (individual + combined)
-- Make a Tool() class - implements name, usage notes
-- Make a Process() class and library - implements name, description, hardware + room dependencies, complexity
-- Code the CLI boilerplate for Deployer.py
+- Write the Engineer() and Group() classes and library - implements name, year, familiar tools, experience, availability (individual + combined)
+- Write the Tool() class and library - implements name, usage notes
+- Write the Process() class and library - implements name, description, hardware + room dependencies, complexity
+- Write the CLI boilerplate for Deployer.py
 
 
 Dynamic Gannt chart:
 - Integrate Microsoft Calendar
 - Integrate with Excel/Teams/Power Apps/etc
-- Make a Room() class and library - implements number, building, address, max. occupancy, Tools dictionary (Tool:Quantity)
+- Write the Room() class and library - implements number, building, address, max. occupancy, Tools dictionary (Tool:Quantity)
 
 
 -----------------------------
