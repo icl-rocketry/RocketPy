352cac43f20b710b50228b649c0ef78c61c6eb25
raihaan123 <raihaan.usman@gmail.com>
2021-07-10 17:29:25 +0100

    ;khasdfhj;ads

--- rpycore/libraries/tools/Tool.py
+++ rpycore/libraries/tools/Tool.py
@@ -1,33 +1,17 @@
-# Component superclass definition
+# Tool class definition
 # Maintained by Raihaan Usman and Luis Marques
 
 import pickle
 
 
-class Component:
+class Tool:
     
-    def __init__(self, name):
+    def __init__(self, name, usage_notes=""):
         
         # Defined things
         self.name = name
-        self.material = "None"
-
-        # Derived things
-        self.top_position = []                                                      # [X, r and phi] coordinate representation
-        self.density = 0
-        self.mass = 0
-        self.parameters = {}                                                        # {Parameter_name: [value, units]}
-        self.subparts = []
-        self.id = None
-
-
-    def load_parameters(self, parameters):
-        self.parameters = self.parameters | parameters                              # For Python>=3.9 - not backwards compatible!!
-
-
-    def add_subpart(self, subpart):
-        self.subparts.append(subpart)                                               # List of child component objects
-
+        self.usage_notes = usage_notes
+        
 
     def export(self, path="./exports/"):
         pickle.dump(self, open(path+self.name+"_"+self.id+".", "wb"))
