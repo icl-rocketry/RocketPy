fa8fd3246e77687cc2b15d05dd1083bd12115662
kn319 <kn319@ic.ac.uk>
2021-07-10 19:20:49 +0400

    Updated Deployer

--- rpycore/Deployer.py
+++ rpycore/Deployer.py
@@ -74,11 +74,14 @@ Dynamic Gantt chart:
 
 '''
 
+
 import pickle
 import libraries.toolbox as toolbox
-import libraries.rockets.Rocket as Rocket
-import libraries.components.Component as Component
-import libraries.interfaces.Interface as Interface
+import libraries.processes.Process as Process
+import libraries.engineers.Engineer as Engineer
+import libraries.engineers.Group as Group
+import libraries.tools.Tool as Tool
+import libraries.rooms.Room as Room
 
 
 # Load rocket object
@@ -86,86 +89,215 @@ rocket_name = input("\nEnter the name of your rocket: ")
 rocket = toolbox.rocket_load(rocket_name)
 
 
-
-manufacturing_processes=['Process1','Process2','Process3','Custom'] #List of given manufacturing processes + 1 custom process
-integration_processes=['Process1','Process2','Process3','Custom'] #List of given integration processes + 1 custom process
+manufacturing_processes=['Process1','Process2','Process3','Custom','Done']                                              # List of given manufacturing processes + 1 custom process
+integration_processes=['Process1','Process2','Process3','Custom','Done']                                                # List of given integration processes + 1 custom process
 
 print("--------Components--------")
 
 
-for component in Rocket.components:
-    comp_obj = Component(component)  # creates a component object
+for component in rocket.components:
 
     while True:
 
-        print(f"For component ({component}), select a manufacturing process:")
+        print(f"For component ({component.name}), select a preset manufacturing process, or make custom:")
         print(manufacturing_processes)
 
-        ch=int(input("Choose a process:")) #Selecting which process to add to the Component's "processes" list
+        ch = int(input("Choose a process:"))                                                                            # Selecting which process to add to the Component "processes" list
 
-        #The whole "prcoessess" list defines 1 job
+        # The whole "prcoessess" list defines 1 job
 
-        if ch==4:
-            custom=input("Enter custom process: ")
-            comp_obj.processes.append(custom) #Adding to the processess list
+        if ch == len(manufacturing_processes) - 1:                                                                      #If it is a Custom Process
 
+            #Specify name:
+            name = input("Name this process: ")
+            #Specify description:
+            desc = input("Add a generic description for this process: ")
+            #Specify extra_description:
+            extra_desc = input("Add a specific description for this component: ")
+
+            process=Process(name, desc, extra_desc) #Create a process object
+
+            '''
+            #Specify tools:
+            print("----------Tools----------")
+            tools=[]
+            while True:
+                ch1=int(input("Tool Options: 1.Add tool 2.Delete tool 3.Done"))
+
+                if ch1==1:
+
+                    tool=input("Add tool: ")
+                    tools.append(tool)
+                elif ch1==2:
+
+                    tool=input("Specify tool to delete: ")
+                    if tool not in tools:
+                        print("Tool does not exist in list!")
+                        continue
+                    else:
+                        tools.delete(tool)
+                else:
+                    break
+            process.add_tools(tools)
+
+            #Specify room:
+            print("----------Room----------") #!!!I've left the room to be specified as a string for now but it has to be an object (Pls change later)!!!
+            rooms=[]
+            while True:
+                ch1=int(input("Room Options: 1.Add room 2.Delete room 3.Done"))
+
+                if ch1==1:
+
+                    room=input("Add room: ")
+                    rooms.append(room)
+                elif ch1==2:
+
+                    room=input("Specify room to delete: ")
+                    if room not in rooms:
+                        print("Room does not exist in list!")
+                        continue
+                    else:
+                        rooms.delete(tool)
+                else:
+                    break
+            process.add_rooms(rooms)
+
+            #Specify complexity
+            print("----------Complexity----------")
+            complexity=input("Enter complexity of the process")    
+            process.add_complexity(complexity) #!!!For now I've left it as a simple integer to be specified and not as a fxn of the list of tools complexities (Pls change later)!!!    
+        '''
+            component.add_process(process)                                                      # Adding to the component job (list)
+
+
+        elif ch != len(manufacturing_processes):
+            manufacturing_processes[ch-1].mod_extra_desc(input(("Add a specific description for this component: ")))
+            component.add_process(manufacturing_processes[ch-1])                                                      # Adding to the processess list
+        
         else:
-            comp_obj.processes.append(manufacturing_processes[ch-1]) #Adding to the processess list
-            continue
-
-        option=input("Add new process? (Y/N)")
-
-        if option.upper()=="Y":
-            continue
-        elif option.upper()=="N":
             break
 
 
-    comp_obj.target_duration=int(input("Enter target duration (in hours): "))
-    comp_obj.priority=int(input("Enter priority score: 1.High 2. Medium 3. Low"))
-    comp_obj.complexity=int(input("Enter complexity score"))
-
-
+    component.target_duration = int(input("Enter target duration (in hours): "))
+    component.priority = int(input("Enter priority score: 1. High 2. Medium 3. Low"))
+    component.complexity = int(input("Enter complexity score"))
 
 
 print("--------Interfaces--------")
 
-for interface in Rocket.interfaces:
-    interface_obj=Interface(interface)
+for interface in rocket.interfaces:
 
     while True:
 
-        print(f"For interface {interface}, select an integration process: ")
+        print(f"For interfaces ({interface.name}), select a preset integration process, or make custom:")
         print(integration_processes)
 
-        ch = int(input("Choose a task:"))  # Selecting which process to add to the Interface's "processes" list
+        ch = int(input("Choose a process:"))                                                                            # Selecting which process to add to the Component "processes" list
 
-        # The whole "processes" list defines 1 job
+        # The whole "prcoessess" list defines 1 job
 
-        if ch==4:
-            custom=input("Enter custom process: ")
-            interface_obj.processes.append(custom) #Adding to the tasks list
+        if ch == len(integration_processes) - 1:
+            name = input("Name this process: ")
+            desc = input("Add a generic description for this process: ")
+            extra_desc = input("Add a specific description for this interface: ")
 
+            #The below was copied from the above component section (IT WILL OBVIOUSLY HAVE TO BE EDITED):
+            '''
+            #Specify tools:
+            print("----------Tools----------")
+            tools=[]
+            while True:
+                ch1=int(input("Tool Options: 1.Add tool 2.Delete tool 3.Done"))
+
+                if ch1==1:
+
+                    tool=input("Add tool: ")
+                    tools.append(tool)
+                elif ch1==2:
+
+                    tool=input("Specify tool to delete: ")
+                    if tool not in tools:
+                        print("Tool does not exist in list!")
+                        continue
+                    else:
+                        tools.delete(tool)
+                else:
+                    break
+            process.add_tools(tools)
+
+            #Specify room:
+            print("----------Room----------") #!!!I've left the room to be specified as a string for now but it has to be an object (Pls change later)!!!
+            rooms=[]
+            while True:
+                ch1=int(input("Room Options: 1.Add room 2.Delete room 3.Done"))
+
+                if ch1==1:
+
+                    room=input("Add room: ")
+                    rooms.append(room)
+                elif ch1==2:
+
+                    room=input("Specify room to delete: ")
+                    if room not in rooms:
+                        print("Room does not exist in list!")
+                        continue
+                    else:
+                        rooms.delete(tool)
+                else:
+                    break
+            process.add_rooms(rooms)
+
+            #Specify complexity
+            print("----------Complexity----------")
+            complexity=input("Enter complexity of the process")    
+            process.add_complexity(complexity) #!!!For now I've left it as a simple integer to be specified and not as a fxn of the list of tools complexities (Pls change later)!!!    
+        '''
+            interface.add_process(Process(name, desc, extra_desc))                                                      # Adding to the component job (list)
+
+        elif ch != len(integration_processes):
+            integration_processes[ch-1].mod_extra_desc(input(("Add a specific description for this interface: ")))
+            interface.add_process(integration_processes[ch-1])                                                          # Adding to the processess list
+        
         else:
-            interface_obj.processes.append(integration_processes[ch-1]) #Adding to the tasks list
-            continue
-
-        option=input("Add new process? (Y/N)")
-
-        if option.upper()=="Y":
-            continue
-        elif option.upper()=="N":
             break
 
-    interface_obj.target_duration = int(input("Enter target duration (in hours): "))
-    interface_obj.priority = int(input("Enter priority score: 1.High 2. Medium 3. Low"))
-    interface_obj.complexity = int(input("Enter complexity score"))
+
+    interface.target_duration = int(input("Enter target duration (in hours): "))
+    interface.priority = int(input("Enter priority score: 1. High 2. Medium 3. Low"))
+    interface.complexity = int(input("Enter complexity score"))
 
 
 
 #Job Assigner
-
+print("x----------Job Assigner----------x")
 
 
 #Assembly Guide
+
+#Note: I'm assumming we have to ordere the interface processes (given above). I could be stupid, so recheck later
+
+print("x----------Assembly Guide----------x")
+
+tasks_copy=[] #Dummy List that will contain each interface's job
+tasks=[]      #ORDERED List of each interface's job
+
+for interface in rocket.interfaces:
+    job=interface.job
+    tasks_copy.append(job)
+
+
+print("List of integration tasks:")
+print(f"{i+1}. {tasks_copy[i]} "for i in range(len(tasks_copy)-1))
+
+for i in range(len(tasks_copy)-1):
+    ch=input(f"Choose task (enter the above corresponding number) for Task {i+1}: ")
+    tasks.append(tasks_copy[ch-1])
+
+
+print("Ordered List of Integration Tasks:")
+print(f"{i+1}. {tasks[i]} "for i in range(len(tasks)-1))
+
+
+
+
 
